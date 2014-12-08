6c788854ada35cc6967f45fdab63548dbd7b9c2b
Tevildo <drwilko@gmail.com>
2014-12-08 21:27:30 +0000

    show blocks / shares / imports on home screen

---
+++ src/TBlockSharer/Models/Index.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TBlockSharer.Models
+{
+    public class Index
+    {
+        public IEnumerable<string> BlockedUsers { get; set; }
+        public IEnumerable<string> SharedWithUsers { get; set; }
+        public IEnumerable<string> ImportedUsers { get; set; }
+    }
+}
